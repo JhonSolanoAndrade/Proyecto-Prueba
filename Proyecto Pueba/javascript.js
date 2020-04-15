e95067260b1e70ceaaa88d476c361ef6524ec908
jhon <jhonsolano1@hotmail.com>
2020-04-15 12:39:43 -0500

    PASO NUMERO 2 OTRA RAMA

--- Proyecto Pueba/javascript.js
+++ Proyecto Pueba/javascript.js
@@ -5,6 +5,7 @@ function ejecuta()
     ALERT ("PASO NUMERO 2")
     ALERT ("PASO NUMERO 3")
     ALERT ("PASO NUMERO 1 OTRA RAMA")
+    ALERT ("PASO NUMERO 2 OTRA RAMA")
 }
 
 window.onload = ejecuta;
